3985da0206d3767b6c26bd816d2a404a2c6c7ff2
Remi C <remi.fake@fake.com>
2015-01-15 11:21:59 +0100

    simplified, working !

--- script/octree_ordering_out_of_server.py
+++ script/octree_ordering_out_of_server.py
@@ -11,19 +11,13 @@ import psycopg2
 
 def connect_to_base():
     conn = psycopg2.connect(  
-        database='test_pointcloud'
+        database='vosges'
         ,user='postgres'
         ,password='postgres'
-        ,host='localhost'
-        ,port='5433' ) ; 
+        ,host='172.16.3.50'
+        ,port='5432' ) ; 
     cur = conn.cursor()
-    return conn, cur ;
-    
-def set_search_path(conn,cur):
-    #setting the search path    
-    cur.execute("""SET search_path to lod,benchmark_cassette_2013, public; """); 
-    conn.commit(); 
-
+    return conn, cur ; 
 
 def execute_querry(q,arg_list,conn,cur): 
     
@@ -32,9 +26,7 @@ def execute_querry(q,arg_list,conn,cur):
     conn.commit()
     
 def order_patch_by_octree(conn,cur,ipatch, tot_level,stop_level,data_dim):
-    import numpy as np; 
-    import psycopg2 ;
-    import datetime;  
+    import numpy as np;  
     import octree_ordering;
     round_size = 3 
     #get points  :
@@ -93,21 +85,40 @@ def order_patch_by_octree(conn,cur,ipatch, tot_level,stop_level,data_dim):
     arg_list = [ipatch, np.asarray(the_result)[:,0].tolist(), np.asarray(the_result)[:,1].tolist()  ] ; 
     execute_querry(q,arg_list,conn,cur) ;
     return cur.fetchall()[0] ; 
-    
+
+def simple_order(gid, tot_level,stop_level,data_dim,conn,cur):
+
+    import psycopg2 ; 
+    import octree_ordering;   
+    q = """
+        SELECT gid, patch
+        FROM vosges_2011.las_vosges  
+        WHERE gid = %s
+        """;
+    arg_list = [gid] ; 
+    execute_querry(q,arg_list,conn,cur) ;
+    gid , patch =  cur.fetchall()[0] ;
+    opatch,ppl = order_patch_by_octree(conn,cur, patch, tot_level,stop_level,data_dim)
+    q = """
+        UPDATE vosges_2011.las_vosges SET (patch, points_per_level) = (%s,%s)
+        WHERE gid = %s;
+        """
+    arg_list = [opatch,ppl,gid] ; 
+    execute_querry(q,arg_list,conn,cur) ;
+    return gid ;
+        
 def test_order_by_octree():
     import psycopg2 ;
     import datetime;
-    import octree_ordering;
-    tot_level = 6;
-    stop_level = 4 ;
-    data_dim = 3 ; 
-    time_beginning = datetime.datetime.now() ; 
+    import octree_ordering; 
+    
+    
     print 'starting : %s ' % (datetime.datetime.now()); 
     conn,cur = connect_to_base();
     set_search_path(conn,cur);
     
-    q = """DROP TABLE IF EXISTS test_order_out_of_db ; 
-    CREATE TABLE test_order_out_of_db(
+    q = """DROP TABLE IF EXISTS public.test_order_out_of_db ; 
+    CREATE TABLE public.test_order_out_of_db(
         gid int,
         patch pcpatch(6),
         points_per_level int[]
@@ -117,7 +128,7 @@ def test_order_by_octree():
     
     for i in range(1, 1000):
         if i %10 == 0:
-            print '\t starting loop %s : %s ' % (i,datetime.datetime.now()); 
+            print '\t\t starting loop %s : %s ' % (i,datetime.datetime.now()); 
         q = """
         SELECT gid, patch
         FROM benchmark_cassette_2013.riegl_pcpatch_space  
@@ -130,28 +141,107 @@ def test_order_by_octree():
         opatch,ppl = order_patch_by_octree(conn,cur, patch, tot_level,stop_level,data_dim)
                 
         q = """
-        INSERT INTO test_order_out_of_db VALUES (%s,%s,%s);
+        INSERT INTO public.test_order_out_of_db VALUES (%s,%s,%s);
         """
         arg_list = [gid,opatch,ppl] ; 
         execute_querry(q,arg_list,conn,cur) ;
         
     
-    #print result   
+    #print result   conn,cur = connect_to_base();
+    set_search_path(conn,cur);
+    
+    q = """DROP TABLE IF EXISTS public.test_order_out_of_db ; 
+    CREATE TABLE public.test_order_out_of_db(
+        gid int,
+        patch pcpatch(6),
+        points_per_level int[]
+    ) ; 
+    """ ; 
+    execute_querry(q,[],conn,cur) ;
     
     cur.close()
     conn.close() 
-    print 'ending : %s ' % (datetime.datetime.now()); 
+    print '\t ending : %s ' % (datetime.datetime.now()); 
+     
+#test_order_by_octree()
+  
     
-    time_end = datetime.datetime.now()
     
-    print 'duration : %'% (time_end-time_beginning)
-    
-test_order_by_octree()
 
-02:34:35.491445 
-02:36:50.137871
-2min15 : 1031797
-100Million ps/hr
-1031797 / (2*60+15)
 
-7642*3600
+def batch_LOD_multiprocess(processes,split_number,key_tot_start,key_tot_end,the_step): 
+    """Main function, execute a given query in parallel on server"""
+    import  multiprocessing as mp; 
+    import random;
+    #splitting the start_end into split_number interval
+    subintervals = split_interval_into_smaller_interval(split_number,key_tot_start,key_tot_end,the_step);
+    #shuffle so that subintervals are in random order
+    random.shuffle(subintervals); 
+    
+    #batch_LOD_monoprocess([1,100,1]);
+    #return 
+    #multiprocessing: 
+    pool = mp.Pool(processes); 
+    results = pool.map(batch_LOD_monoprocess, subintervals)
+    return results
+    
+def split_interval_into_smaller_interval(split_number,key_tot_start,key_tot_end, the_step):
+    """ simply takes a big interval and split it into small pieces. Warning, possible overlaps of 1 elements at the beginning/end"""
+    import numpy as np    
+    import math
+    key_range = abs(key_tot_end-key_tot_start)/(split_number *1.0) 
+    interval_to_process = [] ;
+    for i,(proc) in enumerate(np.arange(1,split_number+1)):
+        key_local_start = int(math.ceil(key_tot_start+i * key_range)) ;
+        key_local_end = int(math.trunc(key_tot_start+(i+1) * key_range)); 
+        interval_to_process.append([ key_local_start , key_local_end, the_step])        
+        #batch_LOD_monoprocess(key_min,key_max,key_step):
+    #print interval_to_process
+    return interval_to_process 
+   
+
+def batch_LOD_monoprocess((key_min,key_max,key_step)): 
+    """ this function connect to databse, and execute the querry on the specified gid range, step by step"""    
+    import multiprocessing; 
+    tot_level = 8;
+    stop_level = 6 ;
+    data_dim = 3 ; 
+    #connect to db
+    conn,cur = connect_to_base(); 
+
+    #setting the search path    
+    cur.execute("""SET search_path to vosges , public;"""); 
+    conn.commit(); 
+    
+    
+    i = key_min; 
+    while i <= key_max :
+        simple_order(i, tot_level,stop_level,data_dim,conn,cur) ;
+        i+=key_step ; 
+        #if i %int(key_max/10.0-key_min/10.0) == 0 : 
+        #    adv = round((1.0*i-key_min)/(key_max*1.0-key_min*1.0),2);
+        #    print '\t %s: %s %s ' % (str(multiprocessing.current_process().name),' '*int(10*adv),str(adv))
+    print '\t %s' % str(multiprocessing.current_process().name) ; 
+    cur.close()
+    conn.close()
+    
+def batch_LOD_multiprocess_test():
+    """ test of the main function, parameters adapted to IGN big server""" 
+    import datetime ; 
+    time_start = datetime.datetime.now(); 
+    print 'starting : %s ' % (time_start); 
+    
+    key_tot_start=8736
+    key_tot_end= 590264 #6554548
+    key_step = 1 ;
+    processes = 15 ;
+    split_number = processes*100; 
+    # creating a table to hold results    
+
+      
+    batch_LOD_multiprocess(processes,split_number,key_tot_start,key_tot_end,key_step); 
+    time_end = datetime.datetime.now(); 
+    print 'ending : %s ' % (time_end); 
+    print 'duration : %s ' % (time_end-time_start)
+    
+#batch_LOD_multiprocess_test();
